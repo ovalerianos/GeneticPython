55a47392690c09df97a6551d93e5dfc5d6af9d35
Omar Valeriano <ovalerianos@gmail.com>
2016-06-13 11:32:02 -0500

    Update1
    Update1

--- Robot.py
+++ Robot.py
@@ -8,8 +8,8 @@ class Robot:
  y_position = 0
  maxMoves   = 0
  moves      = 0
- sensore_value  = 0
- sensore_actions = range(1)
+ sensor_value  = 0
+ sensor_actions = range(1)
  maze = Maze(np.array([0,0],dtype=int))
  route = range(1)
  heading   = Direction.UNKNOWN
@@ -23,10 +23,35 @@ class Robot:
   self.maze = maze
 
  def run():
- 	self.moves = self.moves + 1
+ 	while True:
+ 		self.moves = self.moves + 1
+ 		if self.get_next_action() == 0:
+ 			return
+ 		if self.maze.get_position_value(x,y) == 4:
+ 			return
+ 		if self.moves > self.maxMoves:
+ 			return
+ 		#self.ma
+
+
+ def make_next_action(self):
+
+ 	if self.get_next_action() == 1:
+ 		currentX = self.x_position
+ 		currentY = self.y_position
+
+ 	if Direction.NORTH == self.heading:
+ 		self.y_position = y_position - 1
+ 		if self.y_position < 0:
+ 			self.y_position = 0
+ 	elif Direction.EAST == self.heading:
+ 		
+
+
+
 
  def get_next_action(self):
- 	return self.sensore_actions[self.get_sensor_value()]
+ 	return self.sensor_actions[self.get_sensor_value()]
 
  def get_heading(self):
  	return self.heading
@@ -38,13 +63,65 @@ class Robot:
  	return self.lst_sensor_value
 
  def get_sensor_value(self):
- 	if self.sensore_value > -1:
- 		return self.sensore_value
-
+ 	if self.sensor_value > -1:
+ 		return self.sensor_value
  	frontSensor,frontLeftSensor,frontRightSensor,leftSensor,rightSensor,backSensor=False
  	if self.get_heading() == Direction.NORTH:
- 		frontSensor     = self.maze.is_wall(self.x_position , self.y_position-1)
+ 		frontSensor     = self.maze.is_wall(self.x_position ,   self.y_position-1)
  		frontLeftSensor = self.maze.is_wall(self.x_position-1 , self.y_position-1)
+ 		frontRightSensor= self.maze.is_wall(self.x_position+1 , self.y_position-1)
+ 		leftSensor      = self.maze.is_wall(self.x_position-1 , self.y_position)
+ 		rightSensor     = self.maze.is_wall(self.x_position+1 , self.y_position)
+ 		backSensor      = self.maze.is_wall(self.x_position ,   self.y_position+1)
+ 	elif self.get_heading() == Direction.EAST:
+ 		frontSensor     = self.maze.is_wall(self.x_position+1 , self.y_position)
+ 		frontLeftSensor = self.maze.is_wall(self.x_position+1 , self.y_position-1)
+ 		frontRightSensor= self.maze.is_wall(self.x_position+1 , self.y_position+1)
+ 		leftSensor      = self.maze.is_wall(self.x_position ,   self.y_position-1)
+ 		rightSensor     = self.maze.is_wall(self.x_position ,   self.y_position+1)
+ 		backSensor      = self.maze.is_wall(self.x_position-1,  self.y_position)
+ 	elif self.get_heading() == Direction.SOUTH:
+ 		frontSensor     = self.maze.is_wall(self.x_position ,   self.y_position+1)
+ 		frontLeftSensor = self.maze.is_wall(self.x_position+1 , self.y_position+1)
+ 		frontRightSensor= self.maze.is_wall(self.x_position-1 , self.y_position+1)
+ 		leftSensor      = self.maze.is_wall(self.x_position+1 , self.y_position)
+ 		rightSensor     = self.maze.is_wall(self.x_position-1,  self.y_position)
+ 		backSensor      = self.maze.is_wall(self.x_position,    self.y_position-1)
+ 	else:
+ 		frontSensor     = self.maze.is_wall(self.x_position-1,  self.y_position)
+ 		frontLeftSensor = self.maze.is_wall(self.x_position-1 , self.y_position+1)
+ 		frontRightSensor= self.maze.is_wall(self.x_position-1 , self.y_position-1)
+ 		leftSensor      = self.maze.is_wall(self.x_position,    self.y_position+1)
+ 		rightSensor     = self.maze.is_wall(self.x_position,    self.y_position-1)
+ 		backSensor      = self.maze.is_wall(self.x_position+1,  self.y_position)
+ 	sensorVal = 0
+ 	if frontSensor == True:
+ 		sensorVal = sensorVal + 1
+ 	if frontLeftSensor == True:
+ 		sensorVal = sensorVal + 2
+ 	if frontRightSensor == True:
+ 		sensorVal = sensorVal + 4
+ 	if leftSensor == True:
+ 		sensorVal = sensorVal + 8
+ 	if rightSensor == True:
+ 		sensorVal = sensorVal + 16
+ 	if backSensor == True:
+ 		sensorVal = sensorVal + 32
+
+ 	self.sensor_value = sensorVal
+ 	return sensorVal
+
+ 
+ def get_position_value(self,x,y):
+ 	 if x < 0 or y < 0 or x >= len(self.maze) or y >= len(self.maze[0]):
+ 	 	return 1
+
+ 	 return  self.maze[y][x]
+
+ def is_wall(self,x,y):
+ 	return self.get_position_value(x,y) == 1
+
+
 
 
 
